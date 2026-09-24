a9f0fa41b08b4216159968fc41f80348fc71d81b
dannydaniel <dannydakwa@gmail.com>
2026-09-23 23:22:19 -0400

    new push

--- script.py
+++ script.py
@@ -1,2 +1,15 @@
 #this code is a simple example of a Python script that prints "Hello, World!" to the console.
 print ("hello world")
+
+class view:
+    def __init__(self):
+        pass
+
+    def display(self):
+        print("This is a simple view class that can be used to display messages.")
+        tone = "This is a simple tone."
+        print(tone)
+
+    def display_message(self, message):
+        print(f"Message: {message}")  
+              
